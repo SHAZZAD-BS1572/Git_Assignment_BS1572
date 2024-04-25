5e7eecfef9221979da2bb639183852c39228bee5
SHAZZAD-BS1572 <shazzad.mia@brainstation-23.com>
2024-04-25 11:08:01 +0600

    Extend programming problem with Fibonacci sequence calculation

--- basic.program.c
+++ basic.program.c
@@ -1,22 +1,18 @@
-// #include <stdio.h>
-
-// int main() {
-//     // Print the name
-//     printf("Hello, my name is John Doe.\n");
-
-//     return 0;
-// }
 #include <stdio.h>
 
-int factorial(int n) {
-    if (n == 0 || n == 1)
-        return 1;
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
     else
-        return n * factorial(n - 1);
+        return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 int main() {
-    int num = 5;
-    printf("Factorial of %d is %d\n", num, factorial(num));
+    int num = 7;
+    printf("Fibonacci sequence up to %d terms:\n", num);
+    for (int i = 0; i < num; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    printf("\n");
     return 0;
 }
