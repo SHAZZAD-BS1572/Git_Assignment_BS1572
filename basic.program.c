a02d76e3d92143c8ea84c3edb6b368fcd59dcf7f
SHAZZAD-BS1572 <shazzad.mia@brainstation-23.com>
2024-04-25 10:57:29 +0600

    Add factorial calculation function

--- basic.program.c
+++ basic.program.c
@@ -1,8 +1,22 @@
+// #include <stdio.h>
+
+// int main() {
+//     // Print the name
+//     printf("Hello, my name is John Doe.\n");
+
+//     return 0;
+// }
 #include <stdio.h>
 
-int main() {
-    // Print the name
-    printf("Hello, my name is John Doe.\n");
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
 
+int main() {
+    int num = 5;
+    printf("Factorial of %d is %d\n", num, factorial(num));
     return 0;
 }
